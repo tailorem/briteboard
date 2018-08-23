e1e0c1c607040fc27b07e56978f17604b88dbe92
Aaron Ford <aaron@aaronford.net>
2018-08-23 21:49:18 +0000

    added full window size to canvas

--- public/client.js
+++ public/client.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  let canvas = new fabric.Canvas('board')
+
+    let canvas = new fabric.Canvas('board');
+    canvas.setHeight(window.innerHeight);
+    canvas.setWidth(window.innerWidth);
+
+  // let socket = io.connect();
 
   let rect = new fabric.Rect({
     left: 100,
