a4e0a983bc15705fa4bcdabcd1357403b252ecf7
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-24 22:29:43 -0700

    revert changes to server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const app = require('express')();
-const http = require('http');
-const socketIo = require('socket.io');
-const fabric = require('fabric').fabric;
+const express = require('express'),
+    app = express(),
+    http = require('http'),
+    socketIo = require('socket.io'),
+    fabric = require('fabric').fabric;
 
-// start web server on port 3000
-const server = http.createServer(app);
+// start webserver on port 8080
+const server =  http.createServer(app);
 const io = socketIo.listen(server);
 server.listen(3000);
 // add directory with our static files
@@ -16,38 +17,34 @@ var componentHistory = [];
 var client_count = 0;
 // event-handler for new incoming connections
 function updateComponentHistory(changes) {
-  history = componentHistory.find(each => each.id === changes.id);
-  if (history) {
-    history.left = changes.left;
-    history.top = changes.top,
-      history.scaleX = changes.scaleX,
-      history.scaleY = changes.scaleY,
-      history.angle = changes.angle
-  }
+    history = componentHistory.find(each => each.id === changes.id);
+    if(history) {
+        history.left = changes.left;
+        history.top = changes.top,
+        history.scaleX = changes.scaleX,
+        history.scaleY = changes.scaleY,
+        history.angle = changes.angle
+    }
 }
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
-  // first send the history to the new client
-  for (let component of componentHistory) {
-    socket.emit('add_component', component.rawData);
-  }
-  socket.emit('init_session', { nextObjID: client_count++ * 10000 });
-  //    }
+   // first send the history to the new client
+   for (let component of componentHistory) {
+        socket.emit('add_component', component.rawData);
+    }
+    socket.emit('init_session', {nextObjID: client_count++ * 10000});
+//    }
 
-  // add handler for broadcast new component
-  socket.on('push_component', function(data) {
-    componentHistory.push(data)
-    // console.log(data);
-    socket.broadcast.emit('add_component', data.rawData);
-  })
-  socket.on('modify_component', function(data) {
-    // console.log(data);
+    // add handler for broadcast new component
+    socket.on('push_component', function (data) {
+        componentHistory.push(data)
+        // console.log(data);
+        socket.broadcast.emit('add_component', data.rawData);
+    })
+    socket.on('modify_component', function (data) {
+        // console.log(data);
     socket.broadcast.emit('update_component', data);
-  })
-  socket.on('remove_component', function(data) {
-    componentHistory.filter(each => each.id = data.id)
-    socket.broadcast.emit('turf_component', data);
-  })
+   })
 
 
 });
