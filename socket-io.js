0169ff7937d06264b52793a95e30a69292a4e6b2
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-26 00:22:46 -0700

    listing number of connections on board page

--- socket-io.js
+++ socket-io.js
@@ -1,66 +1,90 @@
 const boards = require('./db/boards');
-const clients = [];
+const clients = {};
+
+getCurrentUsers = (board) => {
+  const currentUsers = [];
+  for (let client in clients) {
+    // console.log(clients[client].boardId, board)
+    if (clients[client].boardId === board) {
+      currentUsers.push(clients[client]);
+    }
+  }
+  return currentUsers;
+}
 
 module.exports = (io/*, dataHelpers*/) => {
 
   // array of all lines drawn
-  var componentHistory = [];
+  const componentHistory = [];
 
   let DEBUG = false;
 
-  // update component history with incoming changes
-  // event-handler for new incoming connections
-  function updateComponentHistory(changes) {
-    history = componentHistory.find(each => each.id === changes.id);
-    if (history) {
-      if (DEBUG) console.log("History changes", changes);
-      if (DEBUG) console.log("History: before", history.comp)
-      history.left = changes.left;
-      history.top = changes.top,
-      history.height = changes.height;
-      history.scaleX = changes.scaleX,
-      history.scaleY = changes.scaleY,
-      history.angle = changes.angle
-    }
-  }
-  function remvoeFromHistory(id) {
-    componentHistory = componentHistory.filter(each => each.id !== id)
-  }
+  // // update component history with incoming changes
+  // // event-handler for new incoming connections
+  // function updateComponentHistory(changes) {
+  //   history = componentHistory.find(each => each.id === changes.id);
+  //   if (history) {
+  //     if (DEBUG) console.log("History changes", changes);
+  //     if (DEBUG) console.log("History: before", history.comp)
+  //     history.left = changes.left;
+  //     history.top = changes.top,
+  //     history.height = changes.height;
+  //     history.scaleX = changes.scaleX,
+  //     history.scaleY = changes.scaleY,
+  //     history.angle = changes.angle
+  //   }
+  // }
+
+  // function removeFromHistory(id) {
+  //   componentHistory = componentHistory.filter(each => each.id !== id)
+  // }
 
   io.on('connection', function(socket) {
-    clients.push(socket.id);
+    const board = (socket.request.headers.referer).split('/').reverse()[0];
+    const client = { name: 'Anon', boardId: board };
+    clients[socket.id] = client;
+    io/*socket.broadcast.to(board)*/.emit('new connection', getCurrentUsers(board));
 
-    if (DEBUG) console.log(boards);
-    // console.log("SOCKET", socket);
-    // const referer = (socket.request.headers.referer).split('/').reverse()[0];
+    socket.on('disconnect', (reason) => {
+      // if (reason === 'io server disconnect') {
+      //   // the disconnection was initiated by the server, you need to reconnect manually
+      //   socket.connect();
+      // }
+      // else the socket will automatically try to reconnect
+      delete clients[socket.id];
+      io/*socket.broadcast.to(board)*/.emit('user disconnected', getCurrentUsers(board));
+    });
+    // console.log(socket.id, board);
+    // socket.broadcast.emit('user connected', socket.id);
 
-    // first send the history to the new client
-    for (let data of componentHistory) {
-      socket.emit('create_component', data);
-    }
 
-    // add handler for broadcast new component
-    socket.on('create_component', function(data) {
-      componentHistory.push(data)
-      socket.broadcast.emit('create_component', data);
-    })
+    // if (DEBUG) console.log(boards);
+    // // console.log("SOCKET", socket);
 
-    socket.on('modify_component', function(data) {
-      updateComponentHistory(data);
-      socket.broadcast.emit('modify_component', data);
-    })
+    // // first send the history to the new client
+    // for (let data of componentHistory) {
+    //   socket.emit('create_component', data);
+    // }
 
-    socket.on('remove_component', function(data) {
-      remvoeFromHistory(data.id)
-      socket.broadcast.emit('remove_component', data);
-    })
+    // // add handler for broadcast new component
+    // socket.on('create_component', function(data) {
+    //   componentHistory.push(data)
+    //   socket.broadcast.emit('create_component', data);
+    // })
 
-    socket.on('path_created', function(data) {
-      componentHistory.push(data)
-      socket.broadcast.emit('path_created', data);
-    })
+    // socket.on('modify_component', function(data) {
+    //   updateComponentHistory(data);
+    //   socket.broadcast.emit('modify_component', data);
+    // })
+
+    // socket.on('remove_component', function(data) {
+    //   removeFromHistory(data.id)
+    //   socket.broadcast.emit('remove_component', data);
+    // })
+
+    // socket.on('path_created', function(data) {
+    //   componentHistory.push(data)
+    //   socket.broadcast.emit('path_created', data);
+    // })
   });
-
-
-
 }
