1d23918f46f667123029600ba3a519f50a684657
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-31 16:47:51 -0700

    add audio to board

--- public/scripts/video.js
+++ public/scripts/video.js
@@ -1,39 +1,20 @@
 $(document).ready(() => {
-  const room = window.location.pathname.split('/')[2];
-  console.log(room);
+  const roomURL = window.location.pathname.split('/')[2];
+
+  var webrtc = new SimpleWebRTC({
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos
+      remoteVideosEl: 'remoteVideos',
+      // immediately ask for camera access
+      autoRequestMedia: true
+  });
+
+  // we have to wait until it's ready
+  webrtc.on('readyToCall', function () {
+      // you can name it anything
+      webrtc.joinRoom(roomURL);
+  });
+
 });
 
-
-// document.addEventListener("DOMContentLoaded", function() {
-
-//   const canvas = document.getElementById('canvas1');
-//   const context = canvas.getContext('2d');
-//   const video = document.getElementById('video1');
-//   const vendorURL = window.URL;
-
-//   navigator.getMedia = navigator.getUserMedia;
-
-//   navigator.getMedia({
-//     video: true,
-//     audio: false
-//   }, function(stream) {
-//     video.src = vendorURL.createObjectURL(stream);
-//     video.play();
-//   }, function(error) {
-//     console.log("An error occurred:", error);
-//     // Error.code?
-//   });
-// });
-
-
-// const mediaSource = new MediaSource();
-// const video = document.createElement('video');
-// video.srcObject = mediaSource;
-
-// const mediaSource = new MediaSource();
-// const video = document.createElement('video');
-// try {
-//   video.srcObject = mediaSource;
-// } catch (error) {
-//   video.src = URL.createObjectURL(mediaSource);
-// });
