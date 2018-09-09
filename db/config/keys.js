723ced72d63658ccdd792a3d629b97b56ba95ae2
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-09-09 10:49:57 -0700

    add keys example

--- db/config/keys.js
+++ db/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
   mongoURI: 'mongodb://wb_user:rqRP24uX@ds133152.mlab.com:33152/whiteboard-db'
-}
+};
---
+++ db/config/keys_example.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'your mongoDB URI goes here'
+};
